b5829cd41e6a62210f3d99fafa6a49dc9f2f2f8f
kaka2634 <996529090@qq.com>
2021-05-11 23:37:04 +0800

    add income calculation

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,31 +10,70 @@ Page({
     loanAmountDevideTenThousand: 50,
     loanYear: 30,
     yearlyInterestRateMultiHundred: 4.8,
+    monthlyIncome: 0,
+    remainingCashDevideTenThousand: 0,
+    monthlySalaryDevideThousand: 0,
+    yearlyROIRateMultiHundred: 0.0,
     equalPrincipalMessage: '',
     equalInterestMessage: '',
-    showClearButton: false
+    equalPrincipalTotalIncomeMessage: '',
+    equalInterestTotalIncomeMessage: '',
+
+    showClearButton: false,
+    showTopTips: false,
+    rules: [{
+      name: 'loanAmount',
+      rules: { required: true, message: '贷款金额是必填项' },
+    }, {
+      name: 'loanYear',
+      rules: { required: true, message: '贷款年限是必填项' },
+    }, {
+      name: 'yearlyInterestRate',
+      rules: { required: true, message: '贷款利率是必填项' },
+    }],
+    formatData: {
+
+    }
   },
 
   onLoad() {
-
   },
+
 
   loanAmountChange(e) {
     this.setData({
       loanAmountDevideTenThousand: e.detail.value
-    })
+    });
   },
 
   loanYearChange(e) {
     this.setData({
       loanYear: e.detail.value
-    })
+    });
   },
 
   yearlyInterestRateChange(e) {
     this.setData({
       yearlyInterestRateMultiHundred: e.detail.value
-    })
+    });
+  },
+
+  remainingCashChange(e) {
+    this.setData({
+      remainingCashDevideTenThousand: e.detail.value
+    });
+  },
+
+  monthlySalaryChange(e) {
+    this.setData({
+      monthlySalaryDevideThousand: e.detail.value
+    });
+  },
+
+  yearlyROIRateChange(e) {
+    this.setData({
+      yearlyROIRateMultiHundred: e.detail.value
+    });
   },
 
   submitForm() {
@@ -44,17 +83,46 @@ Page({
     let result1 = util.calculateEqualPrincipal(loanAmount, loanMonth, monthlyInterestRate);
     let result2 = util.calculateEqualInterest(loanAmount, loanMonth, monthlyInterestRate);
     this.setData({
-        equalPrincipalMessage: result1.message,
-        equalInterestMessage: result2.message,
-        showClearButton: true
+      equalPrincipalMessage: result1.message,
+      equalInterestMessage: result2.message,
+      showClearButton: true
     });
+
+    let remainingCash = this.data.remainingCashDevideTenThousand * 10000;
+    let monthlySalary = this.data.monthlySalaryDevideThousand * 1000;
+    let monthlyROIRate = this.data.yearlyROIRateMultiHundred / (12 * 100);
+
+    try {
+      let result3 = util.calculateTotalIncome(result1.monthlyPayments, loanMonth, remainingCash, monthlySalary, monthlyROIRate);
+      this.setData({
+        equalPrincipalTotalIncomeMessage: '\n选择等额本金最后收益:' + result3.message
+      });
+    } catch (error) {
+      this.setData({
+        equalPrincipalTotalIncomeMessage: '\n选择等额本金: \n' + error + '\n'
+      })
+    }
+
+    try {
+      let result4 = util.calculateTotalIncome(result2.monthlyPayments, loanMonth, remainingCash, monthlySalary, monthlyROIRate);
+      this.setData({
+        equalInterestTotalIncomeMessage:  '选择等额本息最后收益:' + result4.message
+      });
+    } catch (error) {
+      this.setData({
+        equalInterestTotalIncomeMessage: '选择等额本息: \n' + error + '\n'
+      })
+    }
+
   },
 
   clearResult() {
     this.setData({
       equalPrincipalMessage: '',
       equalInterestMessage: '',
+      equalInterestTotalIncomeMessage: '',
+      equalPrincipalTotalIncomeMessage:'',
       showClearButton: false
-  });
+    });
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -35,14 +35,47 @@ const calculateEqualInterest = (loanAmount, loanMonth, monthlyInterestRate) => {
   let factor = Math.pow((1 + monthlyInterestRate), loanMonth);
   let monthlyPayment = loanAmount * (monthlyInterestRate * factor) / (factor - 1);
   let loanInterest = monthlyPayment * loanMonth - loanAmount;
+  let monthlyInterests = [];
+  let monthlyPayments = [];
+  for (let i = 0; i < loanMonth; ++i) {
+    monthlyInterests.push(loanInterest);
+    monthlyPayments.push(monthlyPayment);
+  }
   let message = `等额本息：\n`;
   message +=`每月应还 ${monthlyPayment.toFixed(2)}\n`;
-  message +=`总利息： ${loanInterest.toFixed(2)} `;
+  message +=`总利息： ${loanInterest.toFixed(2)}\n `;
   
   let result = {
       monthlyPayment: monthlyPayment,
+      monthlyInterests: monthlyInterests,
+      monthlyPayments: monthlyPayments,
       loanInterest: loanInterest,
       message : message
+  }
+  return result;
+}
+
+const calculateTotalIncome = (monthlyPayments, loanMonth, remainingCash, monthlySalary, monthlyROIRate) => {
+  let totalIncome = remainingCash;
+  let totalSalary = 0.0;
+  let monthlyIncomes = [];
+  for (let i = 0; i < loanMonth; ++i) {
+    totalIncome = totalIncome * (1 + monthlyROIRate);
+    totalSalary += monthlySalary; 
+    totalIncome = totalIncome - monthlyPayments[i] + monthlySalary;
+    monthlyIncomes.push(totalIncome);
+    if (totalIncome < 0) {
+      console.log(totalIncome);
+      throw `没有足够的资金去还第${i}个月贷款`;
+    }
+  }
+  let message = "\n"
+  message +=`个人工资总共所得: ${totalSalary.toFixed(2)} ， 还完贷款最终剩余: ${totalIncome.toFixed(2)} \n`;
+
+  let result = {
+    totalIncome: totalIncome,
+    monthlyIncomes: monthlyIncomes,
+    message: message
   }
   return result;
 
@@ -50,5 +83,6 @@ const calculateEqualInterest = (loanAmount, loanMonth, monthlyInterestRate) => {
 
 module.exports = {
   calculateEqualPrincipal,
-  calculateEqualInterest
+  calculateEqualInterest,
+  calculateTotalIncome
 }
